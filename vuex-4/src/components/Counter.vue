6434958eb83eb2abde463b2d007674382c54048f
xauroxs <xauroxs@gmail.com>
2024-01-02 21:31:40 +0300

    create simple example of vuex store

---
+++ vuex-4/src/components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>Counter component</h2>
+  <p>Counter: {{ count }}</p>
+  <button @click="increment">Increase counter</button>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  methods: {
+    increment() {
+      this.$store.commit("increment");
+    },
+  },
+  computed: {
+    count() {
+      return this.$store.state.count;
+    },
+  },
+};
+</script>
