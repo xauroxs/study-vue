e17d88fc92b135013187581a32d66cabb942a360
xauroxs <xauroxs@gmail.com>
2024-01-04 23:26:06 +0300

    recreate counter component in composition api

--- vuex-4/src/App.vue
+++ vuex-4/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <h1>App component</h1>
   <Counter />
+  <CounterComposition />
   <Todos />
 </template>
 
 <script>
 import Counter from "./components/Counter.vue";
 import Todos from "./components/Todos.vue";
+import CounterComposition from "./components/CounterComposition.vue";
 
 export default {
   name: "App",
-  components: { Counter, Todos },
+  components: { Counter, Todos, CounterComposition },
 };
 </script>
---
+++ vuex-4/src/components/CounterComposition.vue
@@ -0,0 +1,39 @@
+<template>
+  <h2>CounterComposition component</h2>
+  <p>Count: {{ count }}</p>
+  <p>Name: {{ name }}</p>
+  <p>Age: {{ age }}</p>
+  <div>
+    <button @click="incrementCount">Increase count</button>
+    <button @click="decrementCount">Decrease count</button>
+  </div>
+  <div>
+    <input type="text" v-model="newName" />
+    <button @click="() => changeName(newName)">Change name</button>
+  </div>
+  <div>
+    <button @click="incrementAge">Increase age</button>
+    <button @click="decrementAge">Decrease age</button>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+
+const newName = ref("");
+
+const count = computed(() => store.state.count.count);
+const name = computed(() => store.state.user.name);
+const age = computed(() => store.state.user.age);
+
+const incrementCount = () => store.commit("incrementCount");
+const decrementCount = () => store.commit("decrementCount");
+
+const changeName = (newName) => store.commit("changeName", newName);
+
+const incrementAge = () => store.commit("incrementAge");
+const decrementAge = () => store.commit("decrementAge");
+</script>
