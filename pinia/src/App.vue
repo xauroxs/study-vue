e8aa66e840e98e8a23ded966e474755ac92b2402
xauroxs <xauroxs@gmail.com>
2024-01-05 16:27:22 +0300

    create a store & component with options api

--- pinia/src/App.vue
+++ pinia/src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <h1>App component</h1>
+  <Counter />
 </template>
 
 <script>
+import Counter from "./components/Counter.vue";
+
 export default {
   name: "App",
+  components: { Counter },
 };
 </script>
---
+++ pinia/src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>Counter component</h2>
+  <p>Count: {{ count }}</p>
+  <button type="button" @click="incrementCount">Increase count</button>
+  <button type="button" @click="decrementCount">Decrease count</button>
+</template>
+
+<script>
+import { mapState, mapActions } from "pinia";
+
+import { useCounterStore } from "@/stores/counter";
+
+export default {
+  name: "Counter",
+  computed: {
+    ...mapState(useCounterStore, ["count"]),
+  },
+  methods: {
+    ...mapActions(useCounterStore, ["incrementCount", "decrementCount"]),
+  },
+};
+</script>
